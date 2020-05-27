73066fe36584e6e6b4465b3ed912305ea4667738
nor5 <nor5>
2020-05-27 16:13:00 +0200

    options

--- test_myproms/projectItemsManagement/urls.py
+++ test_myproms/projectItemsManagement/urls.py
@@ -6,5 +6,7 @@ urlpatterns = [
     path('AnaQuanti/<int:id>',views.quanti, name='AnaQuanti'),
     path('AllProjects', views.allProject, name='allp'),
     path('AllProjects/<int:id>',views.selectedproject, name='AllProjects'),
-    path('ProtList/<int:id>',views.projectProteinList, name='ProteinList')
+    path('ProtList/<int:id>',views.projectProteinList, name='ProteinList'),
+    path('AllExperiment/<int:id>',views.experiments, name='AllExperiments'),
+    path('Access/<int:id>',views.projectAccess, name='ProjectAccess'),
 ]
--- test_myproms/projectItemsManagement/views.py
+++ test_myproms/projectItemsManagement/views.py
@@ -35,11 +35,11 @@ def selectedproject(request, id):
     try:
 
       Selectproject = Project.objects.filter(id_project=id)
-      proteins= Protein.objects.filter(id_project=id)
-    except project.DoesNotExist:
+     
+    except Project.DoesNotExist:
             raise Http404
 
-    return render (request, 'projectItemsManagement/selectedProject.html', {'selected_Project': Selectproject,'project_proteins': proteins})
+    return render (request, 'projectItemsManagement/selectedProject.html', {'selected_Project': Selectproject})
 
 
 def projectProteinList(request, id):
@@ -48,10 +48,38 @@ def projectProteinList(request, id):
 
       
       proteins= Protein.objects.filter(id_project=id)
-    except project.DoesNotExist:
+    except Protein.DoesNotExist:
             raise Http404
 
     return render (request, 'projectItemsManagement/proteinList.html', {'project_proteins': proteins})
+
+
+def experiments(request, id):
+   
+    try:
+
+      
+      exp= Experiment.objects.filter(id_project=id)
+    except Experiment.DoesNotExist:
+            raise Http404
+
+    return render (request, 'projectItemsManagement/AllExperiments.html', {'project_exp': exp})
+
+
+def projectAccess(request, id):
+   
+    try:
+
+      
+      access= ProjectAccess.objects.filter(id_project=id)
+      userlist=UserList.objects.all()
+    
+      userprofile=UserProfile.objects.all()
+    except ProjectAccess.DoesNotExist:
+            raise Http404
+
+    return render (request, 'projectItemsManagement/projectAccess.html', {'project_access': access, 'user_list':userlist, 'user_profile':userprofile})
+
 
 
 
